f8cfefd57c518e0e120cde713bfcc3e47db00bb5
Nasnini <52823496+Nasnini@users.noreply.github.com>
2020-03-09 08:54:24 +0200

    Update calculator.py

--- string_calculator/calculator.py
+++ string_calculator/calculator.py
@@ -1,38 +1,45 @@
+# Import regular expression to form a search pattern
 import re
 
+# Constructor to compile the regular expression
 regex = re.compile(r'\d+')
 
+# Method to check empty string values
 def has_negatives(string):
     empty_string = ''
 
+    # For loop to search a specific search pattern
     for x in range(len(string)):
         if string[x] == '-' and string[x+1].isdecimal():
             empty_string += '-' + string[x+1] + ','
 
     return empty_string
 
+# Add function 
 def add(string):
     sum = 0
-    numbers = regex.findall(string) # this line is telling the program to find all the regular expressions
+    numbers = regex.findall(string) # Find all regular expressions
     negatives = has_negatives(string)
-        
+    
+    # Try this to check if theres any negative numbers, else raise an exception and print error message
     try:
         string[:-1]
     except:
         raise "This is not ok!"
     if negatives:
-        raise Exception("Negatives are not allowed: " + negatives) # This exception is telling the program that if there is any negative numbers within the program that an error/ exception will be thrown.
+        raise Exception("Negatives are not allowed: " + negatives)
 
+    #If theres an empty string: return 0, else continue checking...
     if string == '':
         return 0
     else:
         for x in numbers:
-
+            # Program is limited to 20 values in this case
             if int(x) > 20:
                 pass
             else:
                 num = int(x)
                 sum += num
-
+        # return sum on the calculator
         return sum
 print(add("1,1"))
